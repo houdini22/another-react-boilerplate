ca7eeafa2ebdb295d36aade5b1506085a30d6c3f
Michał Baniowski <michal.baniowski@gmail.com>
2023-05-12 07:24:05 +0200

    fixed error?

--- API/resources/views/layouts/app.blade.php
+++ API/resources/views/layouts/app.blade.php
@@ -4,10 +4,13 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>{{\Illuminate\Support\Arr::get($meta, 'title')}}</title>
-    <meta name="description" content="{{\Illuminate\Support\Arr::get($meta, 'description')}}">
-    <meta name="keywords" content="{{\Illuminate\Support\Arr::get($meta, 'keywords')}}">
-    <meta name="robots" content="{{\Illuminate\Support\Arr::get($meta, 'robots')}}">
+    @if(isset($meta))
+        <title>{{\Illuminate\Support\Arr::get($meta, 'title')}}</title>
+        <meta name="description" content="{{\Illuminate\Support\Arr::get($meta, 'description')}}">
+        <meta name="keywords" content="{{\Illuminate\Support\Arr::get($meta, 'keywords')}}">
+        <meta name="robots" content="{{\Illuminate\Support\Arr::get($meta, 'robots')}}">
+    @endif
+
     <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
     <script src="{{ asset('build/assets/app.js') }}" defer></script>
 </head>
