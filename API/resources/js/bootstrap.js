4aee7480e4201fdd6f328ca106f48201c5ca7beb
Michał Baniowski <michal.baniowski@gmail.com>
2023-05-12 03:32:10 +0200

    fix in cms edit settings form

--- API/resources/js/bootstrap.js
+++ API/resources/js/bootstrap.js
@@ -1,23 +1,12 @@
-import lodash from 'lodash'
-window._ = lodash
-
-
-import * as Popper from '@popperjs/core'
-window.Popper = Popper
+try {
+    window.$ = window.jQuery = require('jquery');
+    window.Popper = require('@popperjs/core');
+    window.bootstrap = require('bootstrap');
+} catch (e) {
+    console.error(e);
+}
 
 import 'bootstrap'
-
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-import axios from 'axios'
-window.axios = axios
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- WWW/src/js/routes/CmsSettings/components/Edit/EditForm.tsx
+++ WWW/src/js/routes/CmsSettings/components/Edit/EditForm.tsx
@@ -10,7 +10,7 @@ class EditForm extends React.Component<null, null> {
             <form onSubmit={handleSubmit}>
                 <Field name="title" label="Title" type="text" placeholder={'Meta Title'} component={FormField} />
                 <Field name="description" label="Description" type="textarea" placeholder={'Meta Description'} component={FormField} />
-                <Field name="robots" label="Keywords" type="textarea" placeholder={'Meta Keywords'} component={FormField} />
+                <Field name="keywords" label="Keywords" type="textarea" placeholder={'Meta Keywords'} component={FormField} />
                 <Field name="robots" label="Robots" type="text" placeholder={'Meta Robots'} component={FormField} />
                 <ButtonSave />
             </form>
